0bf8631f6a7a724ef9b2df9ca08f1bf5b816268e
Jānis <97699850+janisslsm@users.noreply.github.com>
2023-04-16 02:12:54 +0300

    bump

--- lsplant/build.gradle.kts
+++ lsplant/build.gradle.kts
@@ -112,7 +112,7 @@ publishing {
         register<MavenPublication>("lsplant") {
             group = "org.lsposed.lsplant"
             artifactId = "lsplant"
-            version = "4.0-aliucord.1"
+            version = "4.0-aliucord.2"
             afterEvaluate {
                 from(components.getByName("release"))
                 artifact(symbolsTask)
