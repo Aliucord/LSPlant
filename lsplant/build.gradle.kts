5a5d9302652f24fd1b4f00550fe85df1fe791d73
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-09-16 23:01:32 -0700

    fix(ci): publish to snapshots repo for backwards compatibility

--- lsplant/build.gradle.kts
+++ lsplant/build.gradle.kts
@@ -176,19 +176,30 @@ publishing {
         }
     }
     repositories {
-        val username = System.getenv("MAVEN_RELEASE_USERNAME")
-        val password = System.getenv("MAVEN_RELEASE_PASSWORD")
+        val username = System.getenv("MAVEN_USERNAME")
+        val password = System.getenv("MAVEN_PASSWORD")
+        val releaseUsername = System.getenv("MAVEN_RELEASE_USERNAME")
+        val releasePassword = System.getenv("MAVEN_RELEASE_PASSWORD")
 
+        if (releaseUsername != null && releasePassword != null) {
+            maven {
+                setUrl("https://maven.aliucord.com/releases")
+                credentials {
+                    this.username = releaseUsername
+                    this.password = releasePassword
+                }
+            }
+        }
+
+        // Publish to snapshots repo for backwards compatibility
         if (username != null && password != null) {
             maven {
+                setUrl("https://maven.aliucord.com/snapshots")
                 credentials {
                     this.username = username
                     this.password = password
                 }
-                setUrl("https://maven.aliucord.com/releases")
             }
-        } else {
-            //mavenLocal()
         }
     }
 }
